abbb41dd7c23ab85f77b9c72becedb520ebefca4
Ivan Holub <ivanholub.js@gmail.com>
2020-05-11 23:16:54 +0300

    feat: add 7-1 sass pattern

--- fancy-weather/src/js/index.js
+++ fancy-weather/src/js/index.js
@@ -1,2 +1,17 @@
+// STYLES IMPORTS
+
 import '../css/style.css';
-import '../css/style.scss';
+import '../css/abstracts/_variables.scss';
+import '../css/abstracts/_functions.scss';
+import '../css/abstracts/_mixins.scss';
+import '../css/vendors/_normalize.scss';
+import '../css/base/_base.scss';
+import '../css/base/_fonts.scss';
+import '../css/base/_typography.scss';
+import '../css/base/_helpers.scss';
+import '../css/layout/_header.scss';
+import '../css/layout/_footer.scss';
+import '../css/components/_button.scss';
+import '../css/pages/_home.scss';
+import '../css/themes/_default.scss';
+
